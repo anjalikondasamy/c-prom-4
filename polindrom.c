40f945180903387ebbfce0f691d83b340f401966
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-05 15:47:32 +0530

    polindrom.c
    good

---
+++ polindrom.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[100];
+clrscr();
+printf("Enter the string");
+gets(str);
+if(str==strrev(str));
+{
+printf("polindrom"0;
+}
+else
+{
+printf("Not polindrom");
+}
+}
